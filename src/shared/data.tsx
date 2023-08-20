8a917c81dd5017ce0a2119f523e0df4c8246b183
Baconriver <riverjiangjh@gmail.com>
2023-08-20 23:12:05 +0930

    add one solution

--- src/shared/data.tsx
+++ src/shared/data.tsx
@@ -889,6 +889,15 @@ export const solutionsData: SolutionsProps = {
         alt: '',
       },
     },
+    {
+      title: 'Solution 6',
+      description: 'This is a placeholder for description. Please put some text in here.',
+      order: '6',
+      image: {
+        src: 'https://images.unsplash.com/photo-1619734086067-24bf8889ea7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
+        alt: '',
+      },
+    },
   ],
 };
 
